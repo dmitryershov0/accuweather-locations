246486aaf33bc9b84e1f5676fb3147324e93897d
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-21 14:28:31 +0300

    Initial project

---
+++ Main/ILocationsApi.cs
@@ -0,0 +1,234 @@
+﻿using System.Threading.Tasks;
+
+namespace Accuweather.Locations
+{
+	/// <summary>
+	/// The Locations API allows users to get a location key for their desired location.
+	/// This location key will then be used in the other APIs to retrieve weather data.
+	/// <para>Doc: https://developer.accuweather.com/accuweather-locations-api/apis</para>
+	/// </summary>
+	public interface ILocationsApi
+	{
+		#region List
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="areaCode"></param>
+		/// <param name="offset"></param>
+		/// <returns></returns>
+		Task<string> GetAreaList(string areaCode, int? offset = null);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <returns></returns>
+		Task<string> GetCountryList(string countryCode);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		Task<string> GetRegionList();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="group"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> GetTopCitiesList(int group, bool details = false);
+
+		#endregion List
+
+		#region  AutoComplete
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="searchText"></param>
+		/// <returns></returns>
+		Task<string> AutoCompleteSearch(string searchText);
+
+		#endregion  AutoComplete
+
+		#region Location Key
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="locationKey"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> GetCityNeighbors(string locationKey, bool details = false);
+
+		#endregion Location Key
+
+		#region  TextSearch
+
+		#region CitySearch
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> CitySearch(string searchText, bool details = false,
+		   int? offset = null, string alias = null);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="adminCode"></param>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> CitySearch(string countryCode, string adminCode,
+		   string searchText, bool details = false, int? offset = null, string alias = null);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> CitySearch(string countryCode, string searchText, bool details = false,
+		   int? offset = null, string alias = null);
+
+		#endregion CitySearch
+
+		#region POI Search
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="serachText"></param>
+		/// <param name="type"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> PointsOfInterestSerach(string serachText, string type = null, bool details = false);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="adminCode"></param>
+		/// <param name="serachText"></param>
+		/// <param name="type"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> PointsOfInterestSerach(string countryCode, string adminCode,
+		   string serachText, string type = null, bool details = false);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="serachText"></param>
+		/// <param name="type"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> PointsOfInterestSerach(string countryCode, string serachText,
+		   string type = null, bool details = false);
+
+		#endregion POI Search
+
+		#region Postal Code Search
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> PostalCodeSearch(string searchText, bool details = false);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> PostalCodeSearch(string countryCode, string searchText, bool details = false);
+
+		#endregion Postal Code Search
+
+		#region Text
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> TextSearch(string searchText, bool details = false,
+		   int? offset = null, string alias = null);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> TextSearch(string countryCode, string searchText, bool details = false,
+		   int? offset = null, string alias = null);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="countryCode"></param>
+		/// <param name="adminCode"></param>
+		/// <param name="searchText"></param>
+		/// <param name="details"></param>
+		/// <param name="offset"></param>
+		/// <param name="alias"></param>
+		/// <returns></returns>
+		Task<string> TextSearch(string countryCode, string adminCode, string searchText,
+		   bool details = false, int? offset = null, string alias = null);
+
+		#endregion Text
+
+		#endregion TextSearch
+
+		#region GeoPosition
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="lat"></param>
+		/// <param name="lon"></param>
+		/// <param name="details"></param>
+		/// <param name="topLevel"></param>
+		/// <returns></returns>
+		Task<string> GeoPositionSearch(double lat, double lon,
+		   bool details = false, bool topLevel = false);
+
+		#endregion GeoPosition
+
+		#region IP Address
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="ipAddress"></param>
+		/// <param name="details"></param>
+		/// <returns></returns>
+		Task<string> GeoPositionSearch(string ipAddress, bool details = false);
+		#endregion IP Address
+	}
+}
---
+++ Main/LocationsApi.cs
@@ -0,0 +1,303 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using Accuweather.Core;
+using Accuweather.Core.Helpers;
+
+namespace Accuweather.Locations
+{
+	public class LocationsApi : AccuweatherApiCore, ILocationsApi
+	{
+		#region Pivate
+
+		private const string _url = "http://dataservice.accuweather.com/location/v1";
+		private object GetLanguageObject
+		{
+			get
+			{
+				return new { language = _language };
+			}
+		}
+		private object GetLanguageDetailsObject(bool details)
+		{
+
+			return new { language = _language, details };
+		}
+
+		#endregion
+
+		#region Public
+		public LocationsApi(string apiKey, string language = "") : base(apiKey, language)
+		{
+		}
+
+		#region List
+
+		public async Task<string> GetAreaList(string areaCode, int? offset = null)
+		{
+			var obj = new
+			{
+				language = _language,
+				offset
+			};
+			var url = $"{_url}/adminareas/{areaCode}?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> GetCountryList(string countryCode)
+		{
+			var url = $"{_url}/countries/{countryCode}?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageObject, url));
+		}
+
+		public async Task<string> GetRegionList()
+		{
+			var url = $"{_url}/regions?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageObject, url));
+		}
+
+		public async Task<string> GetTopCitiesList(int group, bool details = false)
+		{
+			var url = $"{_url}/topcities/{group}?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageDetailsObject(details), url));
+		}
+
+
+		#endregion List
+
+		#region  AutoComplete
+
+		public async Task<string> AutoCompleteSearch(string searchText)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language
+			};
+			var url = $"{_url}/cities/autocomplete?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion  AutoComplete
+
+		#region Location Key
+
+		public async Task<string> GetCityNeighbors(string locationKey, bool details = false)
+		{
+			var url = $"{_url}/cities/neighbors/{locationKey}?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageDetailsObject(details), url));
+		}
+
+		#endregion Location Key
+
+		#region  TextSearch
+
+		#region CitySearch
+
+		public async Task<string> CitySearch(string searchText, bool details = false,
+			int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/cities/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> CitySearch(string countryCode, string adminCode,
+			string searchText, bool details = false, int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/countries/{countryCode}/{adminCode}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> CitySearch(string countryCode, string searchText, bool details = false,
+			int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/cities/{countryCode}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion CitySearch
+
+		#region POI Search
+		public async Task<string> PointsOfInterestSerach(string serachText, string type = null, bool details = false)
+		{
+			var obj = new
+			{
+				q = serachText,
+				language = _language,
+				type,
+				details
+			};
+			var url = $"{_url}/poi/search/?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> PointsOfInterestSerach(string countryCode, string adminCode,
+			string serachText, string type = null, bool details = false)
+		{
+			var obj = new
+			{
+				q = serachText,
+				language = _language,
+				type,
+				details
+			};
+			var url = $"{_url}/poi/{countryCode}/{adminCode}/search/?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> PointsOfInterestSerach(string countryCode, string serachText,
+			string type = null, bool details = false)
+		{
+			var obj = new
+			{
+				q = serachText,
+				language = _language,
+				type,
+				details
+			};
+			var url = $"{_url}/poi/{countryCode}/search/?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion POI Search
+
+		#region Postal Code Search
+
+		public async Task<string> PostalCodeSearch(string searchText, bool details = false)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details
+			};
+			var url = $"{_url}/postalcodes/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageObject, url));
+		}
+		public async Task<string> PostalCodeSearch(string countryCode, string searchText, bool details = false)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details
+			};
+			var url = $"{_url}/postalcodes/{countryCode}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion Postal Code Search
+
+		#region Text
+
+		public async Task<string> TextSearch(string searchText, bool details = false,
+			int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(GetLanguageObject, url));
+		}
+		public async Task<string> TextSearch(string countryCode, string searchText, bool details = false,
+			int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/{countryCode}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		public async Task<string> TextSearch(string countryCode, string adminCode, string searchText,
+			bool details = false, int? offset = null, string alias = null)
+		{
+			var obj = new
+			{
+				q = searchText,
+				language = _language,
+				details,
+				offset,
+				alias
+			};
+			var url = $"{_url}/{countryCode}/{adminCode}/search?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion Text
+
+		#endregion TextSearch
+
+		#region GeoPosition
+
+		public async Task<string> GeoPositionSearch(double lat, double lon,
+			bool details = false, bool topLevel = false)
+		{
+			var q = lat.ToString(CultureInfo.InvariantCulture) +
+				$",{lon.ToString(CultureInfo.InvariantCulture)}";
+
+			var obj = new
+			{
+				q,
+				language = _language,
+				details,
+				topLevel
+			};
+
+			var url = $"{_url}/cities/geoposition/search?apikey={_apiKey}";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		#endregion GeoPosition
+
+		#region IP Address
+		public async Task<string> GeoPositionSearch(string ipAddress, bool details = false)
+		{
+			var obj = new
+			{
+				q = ipAddress,
+				language = _language,
+				details
+			};
+			var url = $"{_url}/cities/ipaddress?apikey={_apiKey}";
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+		#endregion IP Address
+
+		#endregion Public
+	}
+}
